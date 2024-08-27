38fc0290b2b2def7629b3ff5e014417eb3604793
Joseph Jamolod <josephjamolod@gmail.com>
2024-08-28 03:37:25 +0800

    change

--- api/index.ts
+++ api/index.ts
@@ -8,7 +8,7 @@ export interface CustomError extends Error {
   statusCode?: number;
 }
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
