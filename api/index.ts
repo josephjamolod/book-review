6796dd17c2b803761ddf68e78a9f8defe9ad6be7
Joseph Jamolod <josephjamolod@gmail.com>
2024-09-30 21:58:45 +0800

    change

--- api/index.ts
+++ api/index.ts
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: ["https://book-review-platform.vercel.app/"], // Replace with your client deployment URL
+  origin: ["https://book-review-platform-livid.vercel.app/"], // Replace with your client deployment URL
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
